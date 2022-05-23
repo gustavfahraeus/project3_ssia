5f2c8b31b865b374aac5826152b8a7daa584a735
Gustav Fåhraeus <g.fahraeus@hotmail.com>
2022-05-23 16:28:07 +0200

    bibblan sesh sista veckan

--- code.m
+++ code.m
@@ -1,47 +1,114 @@
 function code()
-    config_means = sort(conf_of("easy2.jpg"));
-    config_means_2 = sort(conf_of("easy2.jpg"));
+    close all, clc
+   
+    %extracted_means_1 = sort(preprocess_image("albin_bilder/5.jpg"));
+    %extracted_means_2 = sort(preprocess_image("albin_bilder/13.jpg"));
+    %predict(extracted_means_1, extracted_means_2)
     
-    if isequal(size(config_means),size(config_means_2))
-        for i = 1:length(config_means)
-            if abs(config_means_2(i)/config_means(i) - 1) > 0.2
-                disp("Send to cloud, not similar object.")
-                break
-            end
-        end 
-        disp("Everything ok. Continue to next frame.")
-    else 
-        disp("Send to cloud, difference in amount of objects.")
+    
+    evaluate_time()
+    
+    %T = table2array(readtable('figure_images_labels.csv'));
+    %T  = T(:);
+    %disp(length(T))
+    %preds = build_conf_matrix();
+    %preds = preds(:);
+    %C = confusionmat(preds, T);
+    %disp(C);
+end
+
+function t = evaluate_time()
+    ts = []
+    for i = 11:26
+        for j = 11:26
+            path_1 = strcat("figure_images/", int2str(i), ".jpg")
+            path_2 = strcat("figure_images/", int2str(j), ".jpg")
+            tic
+            extracted_means_1 = sort(preprocess_image(path_1));
+            extracted_means_2 = sort(preprocess_image(path_2));
+            predict(extracted_means_1, extracted_means_2);
+            ts(end+1) = toc         
+        end
+    end
+    histogram(ts)
+end 
+
+function predictions = build_conf_matrix()
+    predictions = [16,16]
+    for i = 11:26
+        for j = 11:26
+            path_1 = strcat("figure_images/", int2str(i), ".jpg")
+            path_2 = strcat("figure_images/", int2str(j), ".jpg")
+            extracted_means_1 = sort(preprocess_image(path_1));
+            extracted_means_2 = sort(preprocess_image(path_2));
+            predictions(i-10,j-10) = predict(extracted_means_1, extracted_means_2);
+        end
     end
 end
 
-function m = conf_of(image_path)
+% Stolen https://se.mathworks.com/matlabcentral/answers/450356-how-can-blur-an-image
+function [output] = blur(A,w)
+[row col] = size(A);
+A=uint8(A);
+B=nan(size(A) + (2*w));
+B(w+1:end-w,w+1:end-w)=A;
+output = 0*A;
+for i=w+1:row+w
+  for j=w+1:col+w
+    tmp=B(i-w:i+w,j-w:j+w);
+    output(i-w,j-w)=mean(tmp(~isnan(tmp)));
+  end
+end
+output=uint8(output);
+end
 
+function p = predict(means_1, means_2)
+    if isequal(size(means_1),size(means_2))
+        for i = 1:length(means_1)
+            if abs(means_2(i)/means_1(i) - 1) > 0.2
+                disp("Send to cloud, not similar object.")
+                p = 1
+                return
+            end
+        end 
+        disp("Everything ok. Continue to next frame.")
+        p = 0
+        return
+    else 
+        disp("Send to cloud, difference in amount of objects.")
+        p = 1
+    end
+end
+
+function m = preprocess_image(image_path)
 %*- Read 'Image 1', convert to binary image and show.
 % I1 = imread('blackboard_images/first.jpg');
 I1 = imread(image_path);
-image(I1);
+% image(I1);
 % I1_bw = imbinarize(I1);
-hist = imhist(rgb2gray(I1));
-% hist = (rgb2gray(I1));
-T = otsuthresh(hist);
-I1_bw2 = rgb2gray(I1);
-z = I1_bw2(:,:)>T*255;
 I1_bw = rgb2gray(I1);
-image(I1_bw);
-imshow(z);
 
-BW1 = edge(I1_bw,'Canny', 0.8);
-BW2 = edge(I1_bw,'log');
-imshow(BW1);
+I1_bw_blurred = imfilter(I1_bw, ones(3)/9, "conv");
+hist = imhist(I1_bw_blurred);
+T = otsuthresh(hist);
+z = I1_bw(:,:)>T*255;
+% image(I1_bw);
+% imshow(z);
+
+% BW1 = edge(I1_bw,'Canny', 0.8);
+% BW2 = edge(I1_bw,'log');
+% imshow(BW1);
 
 % Go on to find connected components of the image and then
 % extract the pixel values from there and find the mean and such.
 
-SE = strel('disk', 20);
-erodedBW = imerode(z, SE);
-imshow(erodedBW);   
-erodedBW = logical(erodedBW)
+SE = strel('disk', 15);
+openedBW = imopen(z, SE);
+
+% hold on
+%figure
+%imshow(erodedBW);  
+erodedBW = logical(openedBW);
 
 stats = regionprops('table',erodedBW,'Centroid',...
     'MajorAxisLength','MinorAxisLength');
@@ -50,11 +117,10 @@ centers = stats.Centroid;
 diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
 radii = diameters/2;
 
-hold on
-viscircles(centers,radii);
+%viscircles(centers,radii);
 hold off
 
-CC = bwconncomp(erodedBW)
+CC = bwconncomp(erodedBW);
 pixel_list = CC.PixelIdxList;
 
 I1_bw_flatten = reshape(I1_bw.',1,[]);
@@ -71,6 +137,5 @@ for index_list = pixel_list
     end
 end
 m = means
-disp(T)
 end
 
---
+++ test.m
@@ -0,0 +1,1 @@
+T = readtable('albin_labels.csv','NumHeaderLines',1);
